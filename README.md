3ef5472eb6bcec89781e0bc84f1aa312bedc3a02
xiyueyezibile <2859439384@qq.com>
2023-09-11 19:37:09 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Docoder
+
+node版本:v16.18.1
+
+## 技术栈
+
+- vite
+- react
+- zustand
+
+## 目录结构
+
+```text
+- src
+  - assets 静态资源
+  - components 组件
+    - PlayerSprites 存放人物精灵
+    - EnemySprites 存放敌人精灵
+    - ItemSprites 存放物品精灵
+    - MapSprites 存放地图精灵
+  - constants 常量
+  - contexts 全局状态
+  - types 类型
+```
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteEslint from 'vite-plugin-eslint';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteEslint({ failOnError: false })],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+});
