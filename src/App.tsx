d433ee0f7ae6e124ab75f5b89631549d21b9cd0d
xiyueyezibile <2859439384@qq.com>
2023-09-24 22:00:51 +0800

    feat: add hitTestRectangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Stage, Text } from '@pixi/react';
-import { getDPR } from './utils/getDPR';
 import './App.scss';
-import { remToPx } from './utils/remToPx';
 import { LegacyRef, useEffect, useRef } from 'react';
-import * as PIXI from 'pixi.js';
-import { detectOrient } from './utils/detectOrient';
+import { detectOrient, remToPx, getDPR } from './utils';
+import { Application } from 'pixi.js';
 function App() {
   const stageRef = useRef<unknown>();
   // console.log(stageRef.current.app);
   useEffect(() => {
     if (stageRef.current) {
-      const { current } = stageRef as { current: { app: PIXI.Application } };
+      const { current } = stageRef as { current: { app: Application } };
       if (current) {
         const app = current.app;
         console.log(app);
