8971cf66c23c63ddebd3cac3c2ab0bda3a1b8090
estarhp <3136551640@qq.com>
2023-10-17 13:43:17 +0800

    feat: 整理bug 的生成和消失的逻辑

--- src/components/MonsterSpawner/index.tsx
+++ src/components/MonsterSpawner/index.tsx
@@ -10,36 +10,7 @@ const App = () => {
   // const [MaliciousScrip, setMaliciousScrip] = useState<JSX.Element>(); //恶意代码
   const { monsters_Bug, setMonsters_Bug } = useMonsters();
   const Timecount = useRef(0);
-  const HitMonitor = () => {
-    const already = monsters_Bug.length > 0;
-    if (LanMei.current && !already) return;
-    const newMonsters = monsters_Bug.filter((item: any) => {
-      const X = item.monsterRef.ref.current.x; // 获得当前的坐标
-      const Y = item.monsterRef.ref.current.y;
-      if (item.monsterRef) return !(X < -20 || Y < -20 || Y > window.innerHeight + 20); // 对于超出屏幕的精灵删除
-      return true;
-    });
-    //     if (MaliciousScrip && MaliciousScrip != <></>) MaliciousScripsRef.current.changeBlood(1); //每移除一个精灵，恶意代码血量减一
 
-    // 如果被蓝妹躲过，移除第一个精灵
-    // setMonsters_Bug((prevMonsters) => prevMonsters.slice(1));
-
-    // 执行碰撞检测并过滤掉碰撞的怪物
-    newMonsters.forEach((item: any) => {
-      if (item.monsterRef && hitTestRectangle(LanMei.current.ref.current, item.monsterRef.ref.current).value) {
-        item.monsterRef.changeBlood(1);
-      }
-    });
-
-    setMonsters_Bug(newMonsters);
-    //     if (
-    //       MaliciousScrip != undefined &&
-    //       MaliciousScripsRef.current.blood <= 0 &&
-    //       MaliciousScripsRef.current.x >= window.innerWidth + 10
-    //     ) {
-    //       setMaliciousScrip(<></>);
-    //     }
-  };
   useTick((delta) => {
     Timecount.current += delta;
     if (Timecount.current > 10) {
